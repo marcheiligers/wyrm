ed360eaea814dd39d8e15983d75b1cd439f59e54
Marc Heiligers <marc@eternal.co.za>
2021-07-17 13:14:07 -0700

    Edge and self collision detection. Basic new game, game and game over states

--- app/main.rb
+++ app/main.rb
@@ -1,22 +1,27 @@
 GRID_SIZE = 20
 GRID_WIDTH = $gtk.args.grid.w / GRID_SIZE
 GRID_HEIGHT = $gtk.args.grid.h / GRID_SIZE
+GRID_CENTER = $gtk.args.grid.w / 2
+GRID_MIDDLE = $gtk.args.grid.h / 2
 
 class Snake
   def initialize
-    @direction = :right
-    @speed = 20
-    @logical_x = GRID_WIDTH / 2
-    @logical_y = GRID_HEIGHT / 2
-    @length = 0
-    @body = []
-    @fruit = random_fruit
+    reset
+    @state = :new_game
   end
 
   def tick(args)
-    handle_input(args)
-    handle_move(args)
-    handle_fruit
+    case @state
+    when :new_game, :game_over
+      handle_menu(args)
+    when :game
+      handle_input(args)
+      handle_move(args)
+      handle_collisions
+      handle_fruit
+    end
+
+    args.outputs.primitives << to_p
   end
 
   def handle_input(args)
@@ -48,12 +53,26 @@ class Snake
     end
   end
 
+  def handle_collisions
+    if @body.include?(head)
+      # we crashed into ourselves
+      @state = :game_over
+    elsif @logical_x < 0 || @logical_x > GRID_WIDTH || @logical_y < 0 || @logical_y > GRID_HEIGHT
+      # we crashed into the edge
+      @state = :game_over
+    end
+  end
+
   def handle_fruit
     return unless head == @fruit
 
     @length += 1
     @speed -= 1
     @fruit = random_fruit
+  end
+
+  def handle_menu(args)
+    reset if args.inputs.keyboard.key_up.space
   end
 
   def random_fruit
@@ -63,14 +82,36 @@ class Snake
     ]
   end
 
+  def reset
+    @direction = :right
+    @speed = 20
+    @logical_x = GRID_WIDTH / 2
+    @logical_y = GRID_HEIGHT / 2
+    @length = 0
+    @body = []
+    @fruit = random_fruit
+    @state = :game
+  end
+
   def to_p
-    [section(head, { r: 0, g: 0, b: 0 })] +
-      @body.map { |pos| section(pos) } +
-      [section(@fruit, { r: 200, g: 12, b: 12 })]
+    case @state
+    when :new_game
+      [text('WYRM'), text('Press [SPACE] to play', -50)]
+    when :game
+      [section(head, { r: 0, g: 0, b: 0 })] +
+        @body.map { |pos| section(pos) } +
+        [section(@fruit, { r: 200, g: 12, b: 12 })]
+    when :game_over
+      [text('GAME OVER'), text('Press [SPACE] to play again', -50)]
+    end
   end
 
   def section(pos, color = { r: 12, g: 100, b: 12 })
     { x: pos.x * GRID_SIZE, y: pos.y * GRID_SIZE, w: GRID_SIZE, h: GRID_SIZE }.merge(color).solid
+  end
+
+  def text(str, y_offset = 0)
+    { x: GRID_CENTER, y: GRID_MIDDLE + y_offset, text: str, size_enum: 2, alignment_enum: 1, r: 155, g: 50, b: 50, a: 255, vertical_alignment_enum: 1 }
   end
 
   def head
@@ -82,5 +123,4 @@ $snake = Snake.new
 
 def tick(args)
   $snake.tick(args)
-  args.outputs.primitives << $snake.to_p
 end
